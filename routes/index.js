8174f347a9c0cd9b8ba9bda3c4f9c2719b16a746
Tran Do Thanh Hai <57478186+tdt-hai@users.noreply.github.com>
2020-07-01 11:44:56 +0700

    db

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var user = require('../services/user');
+const asyncHandler = require('express-async-handler');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
+router.get('/', asyncHandler(async function(req, res, next) {
+  const userid = await user.findAll();
   res.send("Đây là trang index");
-});
+}));
 
 module.exports = router;
