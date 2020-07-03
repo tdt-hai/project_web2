2cc9efa0333c9695b69be3555036ad665544057e
Lendy3159 <Lendy3159@users.noreply.github.com>
2020-07-03 14:52:51 +0700

    display list user when find by content(email,username,acount number)

---
+++ routes/findUser.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+const user = require('../services/user');
+const asyncHandler = require('express-async-handler');
+
+router.post('/',asyncHandler(async function (req,res){
+    
+    const contentSearch = req.body.contentSearch;
+    const users = await user.findUserByContent(contentSearch);
+    
+    res.render('findUser', { users });
+}));
+module.exports = router;
