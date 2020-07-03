c52bb3708f5e6e9dabc4175015d05e9fb8b6e1ff
ngndnhnm <57977726+ngndnhnm@users.noreply.github.com>
2020-07-03 14:27:08 +0700

    uploadImage

--- routes/acc_authentication.js
+++ routes/acc_authentication.js
@@ -1,10 +1,49 @@
 var express = require('express');
 var router = express.Router();
 const user = require('../services/user');
+const multer = require('multer');
+const fs = require("fs");
 const asyncHandler = require('express-async-handler');
-const { body, validationResult } = require('express-validator');
 
 router.get('/', asyncHandler (async function(req,res){
     res.render('acc_authentication');
 }))
+
+
+var storage1 = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './public/images');
+  },
+  filename: function (req, file, callback) {
+    callback(null, user.id + '-' + 'frontImage' + '.png');
+  }
+});
+var storage2 = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './public/images');
+  },
+  filename: function (req, file, callback) {
+    callback(null, user.id + '-' + 'backImage' + '.png');
+  }
+});
+//upload mặt trước cmnd
+var frontUpload = multer({ storage : storage1 }).array('frontImage',1);
+router.post('/frontUpload',function(req,res){
+  frontUpload(req,res,function(err) {  
+      if(err) {
+          return res.end("Error uploading file.");
+      }
+      res.end("File is uploaded");
+  });
+});
+//upload mặt sau cmnd
+var backsideUpload = multer({ storage : storage2 }).array('backImage',1);
+router.post('/backsideUpload',function(req,res){
+  backsideUpload(req,res,function(err) {  
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+    });
+  });
 module.exports = router;
