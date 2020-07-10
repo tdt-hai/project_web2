eab082c509aad5bcbc08b3f62214fecd0bf21818
nm-hoang <57977805+nm-hoang@users.noreply.github.com>
2020-07-10 14:23:06 +0700

    edit user
    update user information

--- routes/user_management.js
+++ routes/user_management.js
@@ -7,13 +7,42 @@ router.get('/',asyncHandler(async function profile(req,res){
     res.render('user_management', {listUser});
 }));
 
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
 
-router.get('/:id',asyncHandler(async function(req,res){
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+ return [year, month, day].join('-');
+}
+
+router.get('/:id',asyncHandler(async function profile(req,res){
     const {id} = req.params;
     const user = await User.findUserById(id);
-    //const time = formatDate(conf.timeStarted);
-    res.render('edituser',{user});
+    const time = formatDate(user.date_range);
+    res.render('edituser',{user,time});
 }));
+
+router.post('/:id',asyncHandler(async function profile(req,res){
+    const {id} = req.params;
+    const temp = id;
+    const email = req.body.email;
+    const displayName = req.body.displayName;
+    const paperType = req.body.paperType;
+    const idNo = req.body.idNo;
+    const issued = req.body.issued;
+    console.log(id,issued,paperType);
+    await User.updateUser(id,email,displayName,paperType,idNo,issued);
+    const listUser = await User.findAll();
+    res.redirect('../user_management');
+
+
+
+ }));
 
 
 module.exports = router;
