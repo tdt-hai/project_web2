82018de37235f82a2850db907c04849e89715823
Lendy3159 <Lendy3159@users.noreply.github.com>
2020-08-12 13:47:10 +0700

    show error when user transferring_money and comfirm_transferring_money

--- routes/confirm_transferring_money.js
+++ routes/confirm_transferring_money.js
@@ -8,21 +8,26 @@ const n2vw = require("n2vw");
 const { body, validationResult } = require("express-validator");
 const Account = require("../services/account");
 
+var destinationBankId;
+var destinationAccountId;
+var amount;
+var note;
+const converter = new n2vw();
+var vnd;
+var destinationAccount;
+
 router.get(
     "/",
     asyncHandler(async function (req, res, next) {
-        const destinationBankId = req.session.destinationBankId;
-        const destinationAccountId = req.session.destinationAccountId;
-        var amount = req.session.amount;
+        destinationBankId = req.session.destinationBankId;
+        destinationAccountId = req.session.destinationAccountId;
+        amount = req.session.amount;
         amount = amount.replace(/\,/g, "");
         amount = parseInt(amount, 10);
-        const note = req.session.note;
-        const converter = new n2vw();
+        note = req.session.note;
+        vnd = converter.getFullText(amount);
+        destinationAccount = await User.findUserByAccountNumber(destinationAccountId);
 
-        const vnd = converter.getFullText(amount);
-        console.log(vnd);
-
-        const destinationAccount = await User.findUserByAccountNumber(destinationAccountId);
         res.render("confirm_transferring_money", {
             destinationBankId,
             destinationAccountId,
@@ -30,6 +35,7 @@ router.get(
             note,
             destinationAccount,
             vnd,
+            errOTP_Code: null,
         });
     })
 );
@@ -42,7 +48,7 @@ router.post(
             .notEmpty()
             .custom(async function (otp_code, { req }) {
                 if (otp_code !== req.session.OTP) {
-                    throw Error("OTP not right ! Please enter again");
+                    throw Error("OTP không đúng ! Vui kiểm tra và nhập lại..!");
                 } else {
                     return true;
                 }
@@ -51,27 +57,36 @@ router.post(
     asyncHandler(async function (req, res) {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(422).render("confirm_transferring_money", { errors: errors.array() });
+            var errOTP_Code = null;
+            errors.array().forEach((e) => {
+                if (e.param === "otp_code") {
+                    errOTP_Code = e.msg;
+                }
+            });
+
+            return res.render("confirm_transferring_money", {
+                destinationBankId,
+                destinationAccountId,
+                amount,
+                note,
+                destinationAccount,
+                vnd,
+                errOTP_Code,
+            });
         }
 
-        var amount = req.session.amount;
-        amount = amount.replace(/\,/g, "");
-        amount = parseInt(amount, 10);
         const sourceAccountId = req.currentUser.account_number;
         const currency = "VND";
         const sourceBankId = "ACB";
-        const destinationBankId = req.session.destinationBankId;
-        const destinationAccountId = req.session.destinationAccountId;
-        const note = req.session.note;
 
         //giao dich
         await Account.addMoney(destinationAccountId, amount);
         await Account.subMoney(sourceAccountId, amount);
         await Transaction.saveTransactionHistory(amount, currency, sourceAccountId, sourceBankId, destinationBankId, destinationAccountId, note);
-        // phi giao dich 
-        //const fee = amount * 0.0007;
-        //login.jsawait Account.subMoney(sourceAccountId, fee);
-        
+        // phi giao dich
+        const fee = amount * 0.0007;
+        await Account.subMoney(sourceAccountId, fee);
+        await Transaction.saveTransactionHistory(fee, currency, sourceAccountId, sourceBankId, destinationBankId, null, "phi chuyen tien");
 
         res.redirect("/users");
     })
